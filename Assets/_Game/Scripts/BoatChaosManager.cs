487fbeee6698eeb90241895e91e429b9d3d02f01
Rom <romain.bevillard@wearexone.com>
2021-10-03 21:20:37 +0200

    Boats as kinematic RBs

--- Assets/_Game/Scripts/BoatChaosManager.cs
+++ Assets/_Game/Scripts/BoatChaosManager.cs
@@ -43,7 +43,7 @@ public class BoatChaosManager : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
 
-        //StartCoroutine(SpawnProps());
+        StartCoroutine(SpawnProps());
     }
 
     IEnumerator SpawnProps()
